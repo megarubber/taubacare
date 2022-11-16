efa23a770731f5bc84a7bff9852adceecad6d64f
Guilherme Samuel <guisamuel800@gmail.com>
2022-11-16 15:41:12 -0300

    feat: add the base login file

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/mytextfield.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -10,10 +11,25 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: Text('oi')
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              MyTextField(
+                validatorText: 'eae',
+                validExp: r'',
+                width: size.width * 0.8
+              ),
+              MyTextField(
+                validatorText: 'eae',
+                validExp: r'',
+                width: size.width * 0.8
+              ),
+            ]
+          )
         )
       )
     );
