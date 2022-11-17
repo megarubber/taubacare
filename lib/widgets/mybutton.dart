5edfb36bfebe10663e0f5ee21bc9421e251a1ab2
Guilherme Samuel <guisamuel800@gmail.com>
2022-11-17 16:30:46 -0300

    chore: add new properties for my button class (action & color)

--- lib/widgets/mybutton.dart
+++ lib/widgets/mybutton.dart
@@ -4,11 +4,15 @@ class MyButton extends StatelessWidget {
   final double width;
   final double height;
   final String message;
+  final Color color;
+  final VoidCallback? action;
 
   MyButton({
     required this.width,
     this.height = 50,
-    required this.message
+    required this.message,
+    this.color = const Color(0xFF0000),
+    this.action = null
   });
 
   @override
@@ -17,12 +21,11 @@ class MyButton extends StatelessWidget {
       width: this.width,
       height: this.height,
       child: ElevatedButton(
-        onPressed: () {
-
-        },
+        onPressed: action,
         child: Text(this.message),
         style: ButtonStyle(
           shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(30))),
+          backgroundColor: MaterialStateProperty.all(this.color),
           textStyle: MaterialStateProperty.all(
             TextStyle(fontFamily: 'Poppins', fontSize: 20, fontWeight: FontWeight.w600)
           )
