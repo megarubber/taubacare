870a5fbff6395c92cf0ab07fea2d1959ead0ce4b
Guilherme Samuel <guisamuel800@gmail.com>
2022-11-18 14:30:03 -0300

    feat: add usertype enum and project colors (class file)
    These was added because they probably will be used in the majority of project

---
+++ lib/utilities/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ProjectColors {
+  static const red = Color(0xFFFF472E);
+  static const teal = Color(0xFF00BCD4);
+  static const lowRed = Color(0xFFFFA195);
+}
