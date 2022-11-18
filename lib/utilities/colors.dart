f5094a86d5fae19d94e46b25edf720db04c5f493
Guilherme Samuel <guisamuel800@gmail.com>
2022-11-18 14:44:11 -0300

    chore: add new colors (white and black)
    Despite the two (black & white) exists in material.dart, it's easier when use just one file for colors

--- lib/utilities/colors.dart
+++ lib/utilities/colors.dart
@@ -4,4 +4,6 @@ class ProjectColors {
   static const red = Color(0xFFFF472E);
   static const teal = Color(0xFF00BCD4);
   static const lowRed = Color(0xFFFFA195);
+  static const white = Color(0xFFFFFFFF);
+  static const black = Color(0xFF000000);
 }
