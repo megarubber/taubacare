cb093095b45d48d877a46d21637a612ec7580e5e
Guilherme Samuel <guisamuel800@gmail.com>
2022-11-18 17:05:02 -0300

    feat: implement tab and tabcontroller system

---
+++ lib/screens/home.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import '../utilities/colors.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: Builder(builder: (context) {
+            return IconButton(
+              onPressed: () => Scaffold.of(context).openDrawer(),
+              icon: Icon(Icons.menu)
+            );
+          }),
+          actions: <Widget>[
+            IconButton(
+              onPressed: null,
+              icon: Icon(Icons.manage_search_rounded)
+            ),
+          ],
+          bottom: const TabBar(
+            tabs: <Tab>[
+              Tab(
+                icon: Icon(Icons.library_books_rounded),
+                child: Text('Educação')
+              ),
+              Tab(
+                icon: Icon(Icons.house_rounded),
+                child: Text('Assistência')
+              ),
+              Tab(
+                icon: Icon(Icons.sports_basketball_rounded),
+                child: Text('Esporte')
+              ),
+            ]
+          )
+        ),
+        body: TabBarView(
+          children: <Widget>[
+
+          ]
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                accountName: Text('oi'),
+                accountEmail: Text('eae'),
+                decoration: BoxDecoration(color: ProjectColors.white),
+                currentAccountPicture: CircleAvatar(
+                  radius: 50.0,
+                  backgroundColor: ProjectColors.lowRed
+                )
+              ),
+              ListTile(
+                leading: const Icon(Icons.person),
+                title: const Text('Voltar')
+              ),
+              ListTile(
+                leading: const Icon(Icons.person),
+                title: const Text('Ver Saldo')
+              ),
+            ]
+          )
+        )
+      )
+    );
+  }
+}
