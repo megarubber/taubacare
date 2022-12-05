bd9e5fb5eda63b85a26fb0f7b67be1b8bb8ee1c3
Guilherme Samuel <guisamuel800@gmail.com>
2022-12-05 11:50:16 -0300

    chore: user id system

--- lib/utilities/session.dart
+++ lib/utilities/session.dart
@@ -1,8 +1,10 @@
 class Session {
+	static String id = 'UUUU';
 	static String name = 'user';
 	static String email = 'user@user.com';
 
 	static void reset() {
+		id = 'UUUU';
 		name = 'user';
 		email = 'user@user.com';
 	}
