15e7b64eedcfdd54028085fb5f8eb614341b6785
Guilherme Samuel <guisamuel800@gmail.com>
2022-12-06 11:06:46 -0300

    test: download picture

--- lib/services/storage.dart
+++ lib/services/storage.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_storage/firebase_storage.dart';
 import 'dart:io';
+import 'dart:typed_data';
 import '../utilities/session.dart';
 
 class MyStorage {
@@ -11,11 +12,13 @@ class MyStorage {
 		this.path = 'users/' + imageName;
 		strRef.putFile(file);
 	}
-	
 	/*
-	Unit8List? downloadProfilePicture() {
-		this.path = 'users/' + Session.name;
-
+	Future<Uint8List> downloadProfilePicture() async {
+		this.path = 'users/' + Session.name + '.jpg';
+		const oneMegabyte = 1024 * 1024;
+		final Uint8List? data = await strRef.getData(oneMegabyte);
+		final img = await rootBundle.load('assets/images/user_icon.png').buffer.asUint8List();
+		return data ?? img;
 	}
 	*/
 }
